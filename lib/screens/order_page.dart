d5256992eea577b4409e7af582d1fff8601ff383
sreeragkvivek <sreeragkvivek23@gmail.com>
2022-01-30 22:44:10 +0530

    added Driver cart

---
+++ lib/screens/order_page.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:driversapp/screens/summary_page.dart';
+
+
+
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({ Key? key }) : super(key: key);
+  @override
+  _OrderPageState createState() => _OrderPageState();
+}
+
+
+class _OrderPageState extends State<OrderPage> {
+  get queryString => "Distributor";
+  get queryString1 => "0";
+  get brand=>"";
+
+
+  @override
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text('My Orders'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Distributors").where("Type",isEqualTo:queryString).snapshots(),
+        builder: (BuildContext context,AsyncSnapshot <QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView (
+              children: snapshot.data?.docs.
+              where((QueryDocumentSnapshot<Object?> element) =>
+              element["Route"].contains(queryString1)
+              ).
+              map(
+                      (doc) =>
+                          ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: GestureDetector(
+                                child: const Icon(Icons.shopping_bag),
+
+                            ),
+                            title: Text(doc["Name"]),
+                            subtitle:Text(doc["Brand"]),
+
+                            trailing:  IconButton(
+                              icon:const Icon(Icons.arrow_forward_ios),
+                              onPressed:(){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>summaryPage(docId:doc.id,brand:doc["Brand"])));
+                              },
+                            ),
+                       ),
+              ).toList() as List<Widget>
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/summary_page.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class summaryPage extends StatefulWidget {
+  String docId;
+  String brand;
+  summaryPage({Key? key,required this.docId, required this.brand}) : super(key: key);
+
+  @override
+  State<summaryPage> createState() => _summaryPageState();
+}
+
+class _summaryPageState extends State<summaryPage> {
+
+  get queryString => "Ordered";
+  dynamic data;
+  CollectionReference dist=FirebaseFirestore.instance.collection("Orders");
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+        body: Center(child:StreamBuilder(
+          stream: dist.where("DistributorID",isEqualTo: widget.docId).snapshots(),
+          builder:(BuildContext context,AsyncSnapshot <QuerySnapshot> snapshot){
+            if(!snapshot.hasData){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return ListView(
+                children: snapshot.data?.docs.
+                where((QueryDocumentSnapshot<Object?> element) =>
+                    element["Status"].contains(queryString)
+                ).
+                map(
+                      (doc) =>
+                      ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        leading: GestureDetector(
+                          child: const Icon(Icons.shopping_bag),
+
+                        ),
+                        title: Text(doc["DistributorID"]),
+
+                        trailing:  IconButton(
+                          icon:const Icon(Icons.arrow_forward_ios),
+                          onPressed:(){},
+                        ),
+                      ),
+                ).toList() as List<Widget>
+            );},
+        ),)
+    );
+  }
+}
+
+
+
