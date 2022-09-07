1f0581a905d64243e3f570e491d58cba71174518
sreeragkvivek <sreeragkvivek23@gmail.com>
2022-09-07 23:17:34 +0530

    finished crate page

--- lib/screens/app/crate_page.dart
+++ lib/screens/app/crate_page.dart
@@ -1,19 +1,12 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:driversapp/static_assets/wave_svg.dart';
 import 'package:driversapp/widget/cust_appbar.dart';
 import 'package:driversapp/widget/nav_bar.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 import '../../models/user_stored.dart';
-
-
-
-
-
-
-
 
 class Cratepage extends StatefulWidget {
   const Cratepage({Key? key}) : super(key: key);
@@ -23,10 +16,6 @@ class Cratepage extends StatefulWidget {
 }
 
 class _CratepageState extends State<Cratepage> {
-  @override
-
-
-
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -35,15 +24,14 @@ class _CratepageState extends State<Cratepage> {
       appBar: custAppBar("Crate Management"),
       body: Center(
           child: Column(children: [
-            SizedBox(
-                height: 150,
-                child: Stack(children: [Positioned(top: 0, child: WaveSvg())])),
-            CratePagebody(),
-          ])),
+        SizedBox(
+            height: 150,
+            child: Stack(children: [Positioned(top: 0, child: WaveSvg())])),
+        CratePagebody(),
+      ])),
     );
   }
 }
-
 
 class CratePagebody extends StatefulWidget {
   @override
@@ -52,113 +40,136 @@ class CratePagebody extends StatefulWidget {
 
 class _CratePagebodyState extends State<CratePagebody> {
   @override
+  late String _numofcrates;
+  late String _currDist;
+  late String _value = "";
+  @override
+  void initState() {
+    super.initState();
+    _numofcrates = "";
+    _currDist = "";
+    _value = "";
+  }
+
   Widget build(BuildContext context) {
     Future getCrate() async {
-
       final user_box = await Hive.openBox<UserStore>('user');
-      final driver_route = user_box
-          .getAt(0)
-          ?.route;
+      final driver_route = user_box.getAt(0)?.route;
 
       var distributorSet = <dynamic>[];
+      Map<String, dynamic> map = {};
       await FirebaseFirestore.instance
-          .collection("Distributors").get().
-      then((QuerySnapshot querySnaphot) {
+          .collection("Distributors")
+          .get()
+          .then((QuerySnapshot querySnaphot) {
         querySnaphot.docs.forEach((doc) {
           if (doc["Route"] == driver_route) {
+            var name = doc["Name"];
+            var crates = doc["Crates"];
             distributorSet.add(doc["Name"]);
-
+            map.addEntries([MapEntry(name, crates)]);
           }
         });
       });
-      print(distributorSet);
-      return distributorSet;
+
+      return map;
     }
+
     return FutureBuilder<dynamic>(
       future: getCrate(),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.hasError) {
           return const Text("Something went wrong");
         }
-
-        // if (snapshot.hasData) {
-        //   return const Text("Document does not exist");
-        // }
         if (snapshot.hasData &&
             snapshot.connectionState == ConnectionState.done) {
+          List<String> finalList = [];
+          Map<String, dynamic> finalMap = snapshot.data.cast<String, dynamic>();
+          List<String> crates = [];
 
-          List<String> finalList = snapshot.data.cast<String>();
-          String dropdownValue = finalList[0];
+          finalMap.forEach((key, value) {
+            finalList.add(key);
+          });
+          finalMap.forEach((key, value) {
+            crates.add(value);
+          });
 
-          String CrateRem = "23";
+          _numofcrates = crates[0];
+
           return Container(
+              padding: const EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width, // Full Width of Screen
+              height: 500.0, // Desired Height
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    DropdownSearch<String>(
+                      items: finalList,
+                      validator: (String? item) {
+                        if (item == null)
+                          return "Required field";
+                        else
+                          return null;
+                      },
+                      onChanged: (String? data) {
+                        {
+                          var CrateRem = finalMap['$data'];
+                          _currDist = data!;
+                          _numofcrates = CrateRem;
+                        }
+                        ;
+                      },
+                    ),
+                    const SizedBox(height: 10.0),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          hintText: 'Returned Number of Crates are',
+                          border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 32.0),
+                              borderRadius: BorderRadius.circular(5.0)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1.0),
+                              borderRadius: BorderRadius.circular(5.0))),
+                      onChanged: (value) {
+                        _value = value;
+                      },
+                    ),
+                    const SizedBox(height: 20.0),
+                    MaterialButton(
+                        color: Colors.lightBlueAccent,
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          print(_numofcrates);
+                          print(_currDist);
+                          var final_crate =
+                              int.parse(_numofcrates) + int.parse(_value);
 
-
-            padding: const EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width, // Full Width of Screen
-            height: 500.0, // Desired Height
-            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Text('Distributor', style: TextStyle(fontSize: 18.5)),
-              DropdownButton(
-                isExpanded: true,
-                value: dropdownValue,
-                icon: const Icon(
-                  Icons.keyboard_arrow_down,
-                ),
-                items: finalList.map((String items) {
-                  return DropdownMenuItem(
-                    value: items,
-                    child: Text(items),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text('Crates Received', style: TextStyle(fontSize: 18.5)),
-              const SizedBox(height: 10.0),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    hintText: 'The remaining crates left are $CrateRem',
-                    border: OutlineInputBorder(
-                        borderSide:
-                        const BorderSide(color: Colors.grey, width: 32.0),
-                        borderRadius: BorderRadius.circular(5.0)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                        const BorderSide(color: Colors.grey, width: 1.0),
-                        borderRadius: BorderRadius.circular(5.0))),
-                onChanged: (value) {
-                  //Do something with this value
-                },
-              ),
-              const SizedBox(height: 20.0),
-              MaterialButton(
-                  color: Colors.lightBlueAccent,
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {})
-            ]),
-          );
-
-
-
+                          print(final_crate);
+                          FirebaseFirestore.instance
+                              .collection("Distributors")
+                              .get()
+                              .then((QuerySnapshot querySnapshot) {
+                            querySnapshot.docs.forEach((doc) {
+                              if (doc["Name"] == _currDist) {
+                                doc.reference
+                                    .update({'Crates': '$final_crate'});
+                              }
+                            });
+                          });
+                        })
+                  ]));
         }
         return const CircularProgressIndicator();
       },
     );
   }
 }
-
-
